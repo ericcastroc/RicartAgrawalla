34b7c7b0426547b2cde3486e6332eda015353c52
Eric Castro <eric.henrique@touchcomp.com.br>
2023-10-20 23:53:16 -0300

    Add node in receive

--- Ricart & Agrawalla/src/main/java/com/mycompany/RicartAgrawalla/Receive/Receive.java
+++ Ricart & Agrawalla/src/main/java/com/mycompany/RicartAgrawalla/Receive/Receive.java
@@ -5,7 +5,13 @@
 package com.mycompany.RicartAgrawalla.Receive;
 
 import com.mycompany.RicartAgrawalla.Connection.Connection;
+import com.mycompany.RicartAgrawalla.EnumNodeState.EnumNodeState;
+import com.mycompany.RicartAgrawalla.Node.Node;
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -17,7 +23,7 @@ public class Receive {
 
     ServerSocket listenSocket;
 
-    public Receive(ServerSocket listenSocket) {
+    public Receive(ServerSocket listenSocket, Node node) {
         this.listenSocket = listenSocket;
     }
 
@@ -26,6 +32,20 @@ public class Receive {
             Socket clientSocket = listenSocket.accept();
             System.err.println("Servidor: Conexão estabelecida com: " + clientSocket.getInetAddress());
             new Connection(clientSocket).start();
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            DataOutputStream out = new DataOutputStream(clientSocket.getOutputStream());
+            DataInputStream inp = new DataInputStream(clientSocket.getInputStream());
+
+            if (in.readLine() == EnumNodeState.LIVRE.getDescricao()) {
+                node.s
+            } else {
+                if (in.readLine() == EnumNodeState.OCUPANDO.getDescricao()) {
+
+                } else if (in.readLine() == EnumNodeState.AGUARDANDO.getDescricao()) {
+
+                }
+            }
         }
     }
 }
